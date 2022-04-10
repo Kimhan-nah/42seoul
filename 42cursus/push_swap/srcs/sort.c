fdacadb5db29aff64d37bba330e828499ee81a84
Kimhannah <skqkdldhf98@naver.com>
2022-04-10 17:36:46 +0900

    재귀 호출 수정해야 함

--- 42cursus/push_swap/srcs/sort.c
+++ 42cursus/push_swap/srcs/sort.c
@@ -3,29 +3,62 @@
 
 static void b_to_a(t_stack *a, t_stack *b, int min, int max)
 {
-	int size;
+	int mid_a;
+	int mid_b;
+	int	size;
 
+	mid_b = (min + max) / 2;
+	mid_a = (min + mid_b) / 2;
 	size = max - min + 1;
 	if (size <= 3)
 	{
 		// small_sort();
 		return ;
 	}
+	while (size > 0)
+	{
+		if (b->top->data > mid_b)
+		{
+			rotate(a, b, B);
+		}
+		else
+		{
+			push(b, a);
+			if (a->top->data > mid_a)
+			{
+				rotate(a, b, A);
+			}
+		}
+		size--;
+	}
+
+//	while (rb > 0)
+//	{
+//		reverse(a, b, B);				// rrb
+//		rb--;
+//	}
+//
+//	a_to_b(a, b, mid_a + 1, max);
+//
+//	b_to_a(a, b, min, mid_b);
+//	b_to_a(a, b, mid_b + 1, mid_a);
 
 
 }
 
 static void	a_to_b(t_stack *a, t_stack *b, int min, int max)
 {
-	int pivot_a;
-	int pivot_b;
+	int mid_a;
+	int mid_b;
 	int	size;
+	int	ra;
 	int rb;
 	int pb;
 
-	pivot_a = (min + max) / 2;
-	pivot_b = pivot_a / 2;
+	mid_a = (min + max) / 2;			// pivot_a
+	mid_b = (min + mid_a) / 2;				// pivot_b
 	size = max - min + 1;
+	ra = 0;
 	rb = 0;
 	pb = 0;
 	if (size <= 3)
@@ -35,14 +68,16 @@ static void	a_to_b(t_stack *a, t_stack *b, int min, int max)
 	}
 	while (size > 0)
 	{
-		if (a->top->data > pivot_a)		// ra
+		if (a->top->data > mid_a)		// ra
+		{
 			rotate(a, b, A);
+			ra++;
+		}			
 		else							// pb
 		{
 			push(a, b);
 			pb++;
-//			(b->len)++;
-			if (b->top->data > pivot_b)
+			if (b->top->data > mid_b)
 			{
 				rotate(a, b, B);		// rb
 				rb++;
@@ -50,16 +85,23 @@ static void	a_to_b(t_stack *a, t_stack *b, int min, int max)
 		}
 		size--;
 	}
+
 	while (rb > 0)
 	{
 		reverse(a, b, B);				// rrb
 		rb--;
 	}
-	a_to_b(a, b, pivot_a + 1, max);
-	b_to_a(a, b, min, pivot_a);
+
+	a_to_b(a, b, mid_a + 1, max);
+
+	b_to_a(a, b, min, mid_b);
+	b_to_a(a, b, mid_b + 1, mid_a);
 }
 
 void	sort(t_stack *a, t_stack *b)
 {
-	a_to_b(a, b, 0, a->max);	
+//	t_operations *operations;
+//
+//	operations = (t_operations *)ft_calloc(1, sizeof(t_operations));
+	a_to_b(a, b, 0, a->max);
 }
